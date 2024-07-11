2719e1565915dafb5ed46a7df1400bd44133ba0d
Jiwoong Song <55657581+shortboy7@users.noreply.github.com>
2024-07-11 19:26:55 +0900

    Title: 최고의 33위치, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240711/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static int [] dr = {0, 1, 2, 0, 1, 2, 0, 1, 2};
+    static int [] dc = {0, 0, 0, 1, 1, 1, 2, 2, 2};
+    
+    public static void main(String[] args) throws Exception{
+        int N = Integer.parseInt(br.readLine());
+        int [][] map = new int[N][N];
+        int answer = -1;
+
+        for (int i = 0 ; i < N ; i++) {
+            StringTokenizer colNums = new StringTokenizer(br.readLine());
+            for (int j = 0 ; j < N ; j++) {
+                map[i][j] = Integer.parseInt(colNums.nextToken());
+            }
+        }
+
+        for (int r = 0 ; r < N - 2; r++) {
+            for (int c = 0, count = 0 ; c < N - 2 ; c++) {
+                
+                for (int i = 0 ; i < 9 ; i++) {
+                    if (map[r + dr[i]][c + dc[i]] == 1) {
+                        count += 1;
+                    }
+                }
+                if (count > answer) {
+                    answer = count;
+                }
+            }
+        }
+        sb.append(answer);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        // 여기에 코드를 작성해주세요.
+    }
+}
