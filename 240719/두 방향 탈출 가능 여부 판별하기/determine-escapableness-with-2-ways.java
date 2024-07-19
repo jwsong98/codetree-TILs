45ba1f112cbe0e37ce5a524c58e6c520cffa1233
Jiwoong Song <55657581+shortboy7@users.noreply.github.com>
2024-07-19 16:21:10 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240719/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,63 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
+
+    static class Pos {
+        int r;
+        int c;
+        Pos (int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+    
+    static final int[] dr = {-1, 0};
+    static final int[] dc = {0, +1};
+
+    public static void main(String[] args) throws Exception {
+        StringTokenizer nm = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(nm.nextToken()) - 1;
+        int M = Integer.parseInt(nm.nextToken()) - 1;
+        int [][] map = new int[N][M];
+
+        for (int r = 0 ; r < N ; r++) {
+            StringTokenizer row = new StringTokenizer(br.readLine());
+            for (int c = 0 ; c < M ; c++) {
+                map[r][c] = Integer.parseInt(row.nextToken());
+            }
+        }
+       	
+        Stack<Pos> stk = new Stack<>();
+        boolean [][] visited = new boolean[N][M];
+        boolean success = false;
+
+        if (map[0][0] == 1) {
+            stk.add(new Pos(0, 0));
+            visited[0][0] = true;
+        }
+        while (!stk.isEmpty()) {
+            Pos cur = stk.pop();
+
+            if (cur.r == N - 1 && cur.c == M - 1) {
+                success = true;
+                break;
+            }
+            for (int d = 0 ; d < 2 ; d++) {
+                int nr = cur.r + dr[d], nc = cur.c + dc[d];
+                if (nr < 0 || nr >= N || nc < 0 || nc >= M || visited[nr][nc] || map[nr][nc] == 0) {
+                    continue;
+                } 
+                visited[nr][nc] = true;
+                stk.push(new Pos(nr, nc));
+            }
+        }
+        sb.append(success?1:0);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
