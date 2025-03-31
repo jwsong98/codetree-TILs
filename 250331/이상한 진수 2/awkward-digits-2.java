2bec44cf0562884ab0f02997eb744080aec714bc
Jiwoong Song <55657581+jwsong98@users.noreply.github.com>
2025-03-31 22:26:00 +0900

    Title: 이상한 진수 2, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

--- 250331/이상한 진수 2/awkward-digits-2.java
+++ 250331/이상한 진수 2/awkward-digits-2.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
 public class Main {
+    static int tryChange(char [] a, int pos) {
+       int result = 0;
+        for (int i = 0 ; i < a.length ; i++) {
+            result <<= 1;
+            int tV = 0, fV = 1;
+            if (pos == i) {
+                tV = 1;
+                fV = 0;
+            }
+            result |= (a[i] == '0') ? tV : fV;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char [] a = sc.next().toCharArray();
 
+        int result = Integer.MIN_VALUE;
         for (int i = 0 ; i < a.length ; i++) {
-            if (a[i] == '0') {
-                a[i] = '1';
-                break;
-            }
-            if (i == (a.length - 1)) {
-                a[i] = '0';
-            }
-        }
-        int result = 0;
-        for (char ch : a) {
-            result <<= 1;
-            result |= (ch == '0') ? 0 : 1;
+            result = Math.max(result, tryChange(a, i));
         }
         System.out.print(result);
     }
