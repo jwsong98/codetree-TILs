a8e095dfea703ee8f91bc8130414e9b3a0724cc1
Jiwoong Song <55657581+jwsong98@users.noreply.github.com>
2025-04-07 22:28:40 +0900

    Title: Carry 피하기 2, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250407/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main {
+    static int answer = Integer.MAX_VALUE;
+
+    static void comb(int [] arr, int idx, int [] picked, int chosen) {
+        if (chosen == 3) {
+            int [] copyed = new int [3];
+            for (int i = 0 ; i < 3; i++) {
+                copyed[i] = picked[i];
+            }
+
+            for (int i = 0 ; i < 5; i++) {
+                int term = 0;
+                for (int j = 0 ; j < 3 ; j++) {
+                    term += (copyed[j] % 10);
+                    copyed[j] /= 10;
+                }
+                if (term / 10 != 0) {
+                    return;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0 ; i < 3 ; i++) {
+                sum += picked[i];
+            }
+            answer = Math.min(answer, sum);
+            return;
+        }
+        for (int i = idx ; i < arr.length; i++) {
+            picked[chosen] = arr[i];
+            comb(arr, i + 1, picked, chosen + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int [] picked = new int [3];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        comb(arr, 0, picked, 0);
+        System.out.print(answer);
+    }
+}
