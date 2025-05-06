228c6ffc1097628af247b7a4bbdc64a2c8ba2265
Jiwoong Song <55657581+jwsong98@users.noreply.github.com>
2025-05-06 22:40:56 +0900

    Title: 2개씩 그룹짓기 2, Time: 1423ms, Memory: 24MB, Status: Wrong Answer - Codetree

--- 250506/2개씩 그룹짓기 2/group-of-pairs-2.java
+++ 250506/2개씩 그룹짓기 2/group-of-pairs-2.java
@@ -38,7 +38,7 @@ public class Main {
         // System.out.println();
 
         int answer = 0;
-        int l = 1, r = 100_000;
+        int l = 1, r = 1000000000;
         while (l <= r) {
             int m = (l + r) / 2;
 
