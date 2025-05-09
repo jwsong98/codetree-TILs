74b92b75c0cb6508f2c374cd416901d7f43e0ced
Jiwoong Song <55657581+jwsong98@users.noreply.github.com>
2025-05-09 15:25:25 +0900

    Title: 2개씩 그룹짓기 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250507/2개씩 그룹짓기 2/group-of-pairs-2.java
+++ 250507/2개씩 그룹짓기 2/group-of-pairs-2.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    static int find(int [] arr, boolean [] visited, int left, int right, int target) {
+    static int find(int [] arr, int left, int right, int target) {
         int l = left, r = right;
 
         int index = -1;
@@ -10,9 +10,7 @@ public class Main {
             int m = (l + r) / 2;
 
             if (target <= arr[m]) {
-                if (index == -1 || index > m) {
-                    index = m;
-                }
+                index = m;
                 r = m - 1;
             } else {
                 l = m + 1;
@@ -21,37 +19,57 @@ public class Main {
         return index;
     }
 
-    static int test(int [] arr, int bar) {
-        int actual = Integer.MAX_VALUE;
+    static boolean test(int [] arr, int bar) {
         boolean [] visited = new boolean [arr.length];
-        int pair = 0;
 
+        int pair = 0;
         for (int i = 0 ; i < arr.length; i++) {
             if (visited[i]) continue;
 
             visited[i] = true;
-            boolean matched = false;
-            int search = find(arr, visited, i + 1, arr.length - 1, arr[i] + bar);
+            int search = find(arr, i + 1, arr.length - 1, arr[i] + bar);
+    
             if (search == -1) {
                 return -1;
             }
-            for (int j = search; j < arr.length; j++) {
+
+            int j = search;
+            for (; j < arr.length; j++) {
                 if (visited[j]) {
                     continue;
                 }
                 visited[j] = true;
-                matched = true;
-                actual = Math.min(actual, arr[j] - arr[i]);
                 break;
             }
-            if (!matched) return -1;
+
+            if (j == arr.length) {
+                if (bar == 2_500_000_00) {
+                    System.out.print("Not visited: ");
+                    int count = 0;
+                    for (int k = search; k < arr.length; k++) {
+                        if (!visited[k] && count < 10) {
+                            System.out.printf("%d ", k);
+                            count += 1;
+                        }
+                    }
+                    System.out.println();
+                }
+                // 23560
+                System.out.printf("%d %d (%d %d) %d %d\n", i, search, arr[search - 1], arr[search], arr[i] + bar, arr[arr.length - 1]);
+                return false;
+            }
             pair += 1;
         }
 
         if (pair != (arr.length / 2)) {
-            return -1;
+            for (int i = 0 ; i < arr.length ; i++) {
+                if (!visited[i]) {
+                    System.out.printf("%d(%d) ", arr[i], i);
+                }
+            }
+            return false;
         }
-        return actual;
+        return true;
     }
 
     public static void main(String[] args) {
@@ -61,25 +79,23 @@ public class Main {
         for (int i = 0; i < 2 * n; i++)
             arr[i] = sc.nextInt();
         Arrays.sort(arr);
-        // for (int i = 0 ; i < arr.length; i++) {
-        //     System.out.printf("%d ", arr[i]);
-        // }
-        // System.out.println();
+
 
         int answer = 0;
         int l = 1, r = 1_000_000_000;
         while (l <= r) {
             int m = (l + r) / 2;
 
-            int temp = test(arr, m);
-            // System.out.printf("%d %d\n", m, temp);
-            if (temp == -1) {
+            boolean res = test(arr, m);
+            if (res) {
+                answer = m;
                 r = m - 1;
             } else {
                 l = m + 1;
-                answer = Math.max(answer, temp);
             }
         }
         System.out.print(answer);
     }
 }
+// 495032915
+// 500000000
